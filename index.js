4b24dd93004657e5a66b61dfe20ffd21ae34258c
ponddevJR <663170010324@rum.ac.th>
2025-03-31 22:40:22 +0700

    remove build

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const { readdirSync } = require("fs");
 require("dotenv").config();
 const connect = require("./config/db");
 
-const server = express();
+const app = express();
 
-server.use(cookieParser());
-server.use(cors({ origin: "http://localhost:5173", credentials: true }));
-server.use("/uploads", express.static(path.join(__dirname, "uploads")));
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
-server.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
 
 connect();
 
@@ -25,4 +25,4 @@ readdirSync("../routes").map((route) => server.use("/api", require("../routes/" 
 // server.listen(PORT, () => console.log("Server is running on port", PORT));
 
 // ✅ ใช้ API Handler ของ Vercel แทน
-export default server;
+module.exports = app;
